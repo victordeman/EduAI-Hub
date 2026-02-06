0909b621f2d416075663999acb895525f7160211
Dr.-Ing. Chukwuka Victor obionwu <victordeman@users.noreply.github.com>
2026-02-06 04:05:18 +0100

    Create tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: 'var(--color-primary-50)',
+          500: 'var(--color-primary-500)',
+          // ...
+        },
+        // Add all vars
+      },
+    },
+  },
+  plugins: [],
+};
