cf0c5f51c00d66e266d6598fd2dbedaca9398301
Dr.-Ing. Chukwuka Victor obionwu <victordeman@users.noreply.github.com>
2026-02-06 04:02:20 +0100

    Create AppLayout.tsx

---
+++ frontend/src/layout/AppLayout.tsx
@@ -0,0 +1,24 @@
+import { useAppStore } from '../lib/script';
+import { Outlet, useLocation } from 'react-router-dom';
+import { Navbar } from './Navbar';
+import { Sidebar } from './Sidebar';
+
+export function AppLayout() {
+  const { isAuthenticated } = useAppStore();
+  const location = useLocation();
+  const showSidebar = isAuthenticated && location.pathname !== '/login';
+
+  return (
+    <div className="min-h-screen bg-[var(--color-bg-dark)]">
+      <Navbar />
+      <div className="flex pt-16">
+        {showSidebar && <Sidebar />}
+        <main className="flex-1 min-h-[calc(100vh-4rem)] p-6 lg:p-8 overflow-auto">
+          <div className="max-w-7xl mx-auto transition-opacity duration-150">
+            <Outlet />
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
